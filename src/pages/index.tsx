35ed5496112bd333e63c18d1a515e1e2d617dd84
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-12-26 16:42:13 +0100

    Inital commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,149 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+import room from "@/assets/images/room.jpg";
+
+import floor1 from "@/assets/images/floor1.png";
+import floor2 from "@/assets/images/floor2.png";
+import floor3 from "@/assets/images/floor3.png";
+
+import wall4 from "@/assets/images/wall4.png";
+import wall5 from "@/assets/images/wall5.png";
+import wall6 from "@/assets/images/wall6.png";
+
+import tile1 from "@/assets/images/tile1.jpg";
+import tile2 from "@/assets/images/tile2.jpg";
+import tile3 from "@/assets/images/tile3.jpg";
+import tile4 from "@/assets/images/tile4.jpg";
+import tile5 from "@/assets/images/tile5.jpg";
+import tile6 from "@/assets/images/tile6.jpg";
+import { useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [side, setSide] = useState("");
+  const [floor, setFloor] = useState("");
+  const [wall, setWall] = useState("");
+
+  function chooseSide(choosen: string) {
+    setSide(choosen);
+  }
+  function chooseFloor(choosen: string) {
+    setFloor(choosen);
+  }
+  function chooseWall(choosen: string) {
+    setWall(choosen);
+  }
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen  items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="py-12 px-12 bg-black/20">
+        <ul className="flex flex-col gap-4">
+          {side === "floor" && (
+            <>
+              {" "}
+              <li
+                onClick={(e) => chooseFloor("tile1")}
+                className="cursor-pointer"
+              >
+                <Image alt="tile1" className="w-20 h-20" src={tile1} />
+              </li>
+              <li
+                onClick={(e) => chooseFloor("tile2")}
+                className="cursor-pointer"
+              >
+                <Image alt="tile1" className="w-20 h-20" src={tile2} />
+              </li>
+              <li
+                onClick={(e) => chooseFloor("tile3")}
+                className="cursor-pointer"
+              >
+                <Image alt="tile1" className="w-20 h-20" src={tile3} />
+              </li>
+            </>
+          )}
+          {side === "wall" && (
+            <>
+              <li
+                onClick={(e) => chooseWall("tile4")}
+                className="cursor-pointer"
+              >
+                <Image alt="tile1" className="w-20 h-20" src={tile4} />
+              </li>
+              <li
+                onClick={(e) => chooseWall("tile5")}
+                className="cursor-pointer"
+              >
+                <Image alt="tile1" className="w-20 h-20" src={tile5} />
+              </li>
+              <li
+                onClick={(e) => chooseWall("tile6")}
+                className="cursor-pointer"
+              >
+                <Image alt="tile1" className="w-20 h-20" src={tile6} />
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      <div className="relative w-4/5 h-4/5 overflow-hidden">
+        <Image alt="Room" src={room} className="w-full h-full" />
+        <div
+          onClick={(e) => chooseSide("wall")}
+          className="0 w-[56%] h-[51.5%] absolute top-[23.5%] left-[22%] z-10 opacity-50 cursor-pointer"
+        ></div>
+
+        {wall === "tile4" && (
+          <Image
+            alt="wall 4"
+            src={wall4}
+            className="w-[100%] h-[100%] absolute top-0"
+          />
+        )}
+        {wall === "tile5" && (
+          <Image
+            alt="wall 4"
+            src={wall5}
+            className="w-[100%] h-[100%] absolute top-0"
+          />
+        )}
+        {wall === "tile6" && (
+          <Image
+            alt="wall 4"
+            src={wall6}
+            className="w-[100%] h-[100%] absolute top-0"
+          />
+        )}
+        <div className="cursor-pointer" onClick={(e) => chooseSide("floor")}>
+          <div className="  w-[64%] h-[24.5%] absolute bottom-[0] right-[4%] z-10 opacity-50 skew-x-[58deg]"></div>
+          <div className=" w-[64%] h-[24.5%] absolute bottom-[0] left-[4%] z-10 opacity-50 -skew-x-[58deg]"></div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {floor === "tile1" && (
+          <Image
+            alt="floor 1"
+            src={floor1}
+            className=" w-[100%] h-[100%] absolute bottom-0"
+          />
+        )}
+        {floor === "tile2" && (
+          <Image
+            alt="floor 1"
+            src={floor2}
+            className=" w-[100%] h-[100%] absolute bottom-0"
+          />
+        )}
+        {floor === "tile3" && (
+          <Image
+            alt="floor 1"
+            src={floor3}
+            className=" w-[100%] h-[100%] absolute bottom-0"
+          />
+        )}
       </div>
     </main>
-  )
+  );
 }
